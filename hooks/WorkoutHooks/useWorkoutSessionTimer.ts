e47596887d4f432c255bf3994ab12244e42c9c1b
Duke <114975455+RaveZam@users.noreply.github.com>
2025-08-06 13:16:43 +0800

    Updated Time Formatter to be compatible with hours

--- hooks/WorkoutHooks/useWorkoutSessionTimer.ts
+++ hooks/WorkoutHooks/useWorkoutSessionTimer.ts
@@ -35,9 +35,12 @@ export const useWorkoutSessionTimer = () => {
   }, [isRunning, activeWorkoutSession]);
 
   const formatTime = (seconds: number) => {
-    const mins = Math.floor(seconds / 60);
+    const hours = Math.floor(seconds / 3600);
+    const mins = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
-    return `${mins}:${secs.toString().padStart(2, "0")}`;
+    return `${hours.toString().padStart(2, "0")}:${mins
+      .toString()
+      .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
   };
 
   const startTimer = () => {
